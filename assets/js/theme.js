b13de8ade6210e6005d7e45f941ce847360a675d
Adam Lewis <lewis.adam19922gmail.com>
2022-05-06 18:52:07 -0700

    Adjusted the services pages header height, fixed the trust pilot logo/BBB Icon, still working on the calculator overhaul, added the calculator to every page.

--- assets/js/theme.js
+++ assets/js/theme.js
@@ -3668,6 +3668,7 @@ $.fn.digits = function () {
 
 $(function () {
   $('header').load('components/head.html');
+  $('calculator-component').load('components/calculator.html');
   $('footer-component').load('components/foot.html');
 });
 /*
--- src/js/custom-template-config.js
+++ src/js/custom-template-config.js
@@ -1,4 +1,7 @@
 $(function() {
     $('header').load('components/head.html');
+    $('calculator-component').load('components/calculator.html');
     $('footer-component').load('components/foot.html');
-})
+
+
+});
